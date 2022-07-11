82ddb96eaa234e8021c1b8db87dbe3874aa734c7
kellyyy666 <107507462+kellyyy666@users.noreply.github.com>
2022-07-11 09:50:17 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,11 +45,11 @@ function draw() {
     nameInput.remove();
     text(".Keep goning, " + you + " . Choose manner of working you want to be ❥" , windowWidth/3, windowHeight/3);
 
-    sendPage = createA('https://kellyyy666.github.io/kellyy66.github.io//_sendInfo/index.html?you='+you, 'Pathfinder');
+    sendPage = createA('https://kellyyy666.github.io/kellyy66.github.io/_sendInfo/index.html?you='+you, 'Pathfinder');
     sendPage.style("font-size: 30px"); 
     sendPage.position((windowWidth/2) - 200, windowHeight*0.6);
 
-    receivePage = createA('https://kellyyy666.github.io/kellyy66.github.io//_receiveInfo/index.html?you='+you, 'Listener');
+    receivePage = createA('https://kellyyy666.github.io/kellyy66.github.io/_receiveInfo/index.html?you='+you, 'Listener');
     receivePage.style("font-size: 30px");
     receivePage.position((windowWidth/2) + 100, windowHeight*0.6);
 
